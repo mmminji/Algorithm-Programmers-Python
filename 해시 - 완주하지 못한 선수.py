8ce9d2cd53ae4de691a4d12606d507de37e1df6f
mmminji <jaellen@korea.ac.kr>
2021-05-11 02:50:44 +0900

    init

---
+++ 해시 - 완주하지 못한 선수.py
@@ -0,0 +1,40 @@
+- 문제 설명
+수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+
+마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+
+# == 나의 풀이 ==
+def solution(participant, completion):
+    
+    participant.sort()
+    completion.sort()
+    
+    pair = zip(participant, completion)
+    for p, c in pair:
+        if p != c:
+            return p
+        
+    return participant[-1]
+
+
+
+# == 모든 풀이 ==
+import collections
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
+
+    
+def solution(participant, completion):
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer
